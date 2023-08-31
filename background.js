90be8a999f03484157fa7bd80f44f67e044ba2ce
phatIntern <phat.pham@saleshood.com>
2023-08-31 10:16:04 +0700

    first commit

---
+++ background.js
@@ -0,0 +1,41 @@
+// background.js
+
+function freezeDOM() {
+  chrome.tabs.query({ active: true, currentWindow: true }, (activeTabs) => {
+    const tabId = activeTabs[0].id;
+    chrome.scripting.executeScript({
+      target: { tabId },
+      function: () => {
+        setTimeout(() => {
+          debugger;
+        });
+      },
+      world: "MAIN",
+      args: ["action"],
+    });
+  });
+
+  
+}
+chrome.commands.onCommand.addListener((command) => {
+  if (command === "freeze-dom") {
+    freezeDOM();
+  }
+});
+
+chrome.runtime.onInstalled.addListener(() => {
+  // Create a context menu item
+  chrome.contextMenus.create({
+    id: "freezeDOM",
+    title: "Freeze DOM",
+    contexts: ["all"], // This makes the context menu option appear for all types of content
+  });
+});
+
+
+// Add a click event listener for the context menu item
+chrome.contextMenus.onClicked.addListener((info) => {
+  if (info.menuItemId === "freezeDOM") {
+    freezeDOM();
+  }
+});
